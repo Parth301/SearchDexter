203ccae55b3f6cd64e7051ae7c27bea244cbbc87
Parth301 <127748701+Parth301@users.noreply.github.com>
2024-12-24 16:26:58 +0530

    Update websites.sql

--- websites.sql
+++ websites.sql
@@ -1,6 +1,6 @@
 CREATE TABLE websites (
     id INT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for each row
-    url VARCHAR(2083) NOT NULL UNIQUE, -- URL of the website (unique and not null)
+    link VARCHAR(2083) NOT NULL UNIQUE, -- URL of the website (unique and not null)
     title VARCHAR(255) NOT NULL,       -- Title of the website (max length 255)
     keywords TEXT,                     -- Keywords associated with the website
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp for record creation
